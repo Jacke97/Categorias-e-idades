4595feb0c4772e697bc7d016a49002696716d8c6
Jacke97 <jafsilva97@hotmail.com>
2021-09-24 18:20:58 -0300

    formulario

---
+++ formulariosimples.php
@@ -0,0 +1,22 @@
+</DOCTYPE html>
+<html>
+
+<head>
+  <meta charset = "utf-8">
+  <title>Formulário de inscrição</title>
+  <meta name= "author" content="">
+  <meta name="viewport" content="width-device-width, initial-scale=1">
+</head>
+
+<body>
+<p>FORMULÁRIO PARA INSCRIÇÃO DE COMPETIDORES</p>
+
+<form action="script.php" method="post">
+  <p>Your name: <Input type="text" name="nome" /></p>
+  <p>Your age: <Input type="text" name="idade" /></p>
+  <p><Input type="submit" value="Enviar dados do competidor" /></p>
+</form>
+
+</body>
+
+</html>
