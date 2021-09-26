2f3764868dbd766a0b0d495077b76c74d30d33ae
Jacke97 <jafsilva97@hotmail.com>
2021-09-26 09:38:34 -0300

    Adicionando validação de campo do formulário

--- script.php
+++ script.php
@@ -27,28 +27,33 @@ if(strlen($nome) > 40)
     return;
 }
 
+if(!is_numeric($idade))
+{   echo "Informe um número para idade";
+    return;
+}
+
 if($idade >= 6 && $idade <= 12)
 {
-    for($i = 0; $i <= count($categorias); $i++)
+    for($i = 0; $i < count($categorias); $i++)
     {
      if($categorias[$i] == 'infantil')
-          echo "O nadador " , $nome, " compete na categoria infantil";
+          echo "O(A) nadador(a) " . $nome. " compete na categoria infantil";
     }
 }
 else if($idade >= 13 && $idade <= 18) 
 {
-    for($i = 0; $i <= count($categorias); $i++)
+    for($i = 0; $i < count($categorias); $i++)
     {
      if($categorias[$i] == 'adolescente')
-          echo "O nadador " , $nome, " compete na categoria adolescente";
+          echo "O(A) nadador(a) " . $nome. " compete na categoria adolescente";
     }
 }
 else
 {  
-    for($i = 0; $i <= count($categorias); $i++)
+    for($i = 0; $i < count($categorias); $i++)
     {
      if($categorias[$i] == 'adulto')
-          echo "O(A) nadador(A) " , $nome, " compete na categoria adulto";
+          echo "O(A) nadador(a) " . $nome. " compete na categoria adulto";
     } 
 }
 
